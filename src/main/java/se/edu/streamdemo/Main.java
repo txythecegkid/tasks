4c8e4f5daec2746541570fb54cca0c26374becf0
XuanYou <tanxuanyou2588@gmail.com>
2026-09-19 17:51:35 +0800

    Work with stream

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -4,6 +4,7 @@ import se.edu.streamdemo.data.Datamanager;
 import se.edu.streamdemo.task.Deadline;
 import se.edu.streamdemo.task.Task;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Main {
@@ -15,21 +16,31 @@ public class Main {
 
         System.out.println("Printing all data ...");
         printAllData(tasksData);
-
+        printAllDataUsingStreams(tasksData);
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
-
+        printDeadlinesUsingStreams(tasksData);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines using stream: " + countDeadlinesUsingStreams(tasksData));
 
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
         int count = 0;
+
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 count++;
             }
         }
+        return count;
+    }
+
+    private static int countDeadlinesUsingStreams(ArrayList<Task> tasks) {
+        int count = (int)tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count();
+
         return count;
     }
 
@@ -46,6 +57,7 @@ public class Main {
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Using iterations");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
